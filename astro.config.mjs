66d89e9acfdbca9e410dab77b19863bdb6f5172a
aponce1509 <aponce1509@hotmail.com>
2024-08-22 14:12:57 +0200

    tutorial ended

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://aponce1509.netlify.app/"
+  site: "https://aponce1509.netlify.app/",
+  integrations: [preact()]
 });
